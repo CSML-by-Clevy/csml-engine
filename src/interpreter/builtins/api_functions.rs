e5628f3e302c93fd07f5dc3a17595ae435b069e2
Alexis Merelo <alexis.merelo@clevy.io>
2019-08-19 16:15:40 +0200

    Update: change reqwest to curl

--- src/interpreter/builtins/api_functions.rs
+++ src/interpreter/builtins/api_functions.rs
@@ -1,14 +1,15 @@
-use serde_json::Value;
-use std::{env, collections::HashMap};
-use reqwest::{ClientBuilder, header::{HeaderMap, HeaderValue, ACCEPT, CONTENT_TYPE}};
+use serde_json::{Value, map::Map};
+use std::{env, collections::HashMap, io::Read};
+use curl::easy::{Easy, List};
+// use reqwest::{ClientBuilder, header::{HeaderMap/, HeaderValue, ACCEPT, CONTENT_TYPE}};
 use crate::parser::{ast::Literal};
 use crate::error_format::data::ErrorInfo;
 use crate::interpreter::{data::Data, builtins::*};
 
 // default #############################################################################
 
-fn parse_api(args: &HashMap<String, Literal>, data: &mut Data) -> Result<(String, HashMap<String, Value>), ErrorInfo> {
-    let mut map: HashMap<String, Value> = HashMap::new();
+fn parse_api(args: &HashMap<String, Literal>, data: &mut Data) -> Result<(String, String), ErrorInfo> {
+    let mut map: Map<String, Value> = Map::new();
 
     if let Some(Literal::StringLiteral{value: fn_id, ..}) = args.get("fn_id") {
         map.insert("function_id".to_owned(), Value::String(fn_id.to_owned()));
@@ -21,57 +22,76 @@ fn parse_api(args: &HashMap<String, Literal>, data: &mut Data) -> Result<(String
 
     map.insert("data".to_owned(), Value::Object(sub_map));
     map.insert("client".to_owned(), Value::Object(client));
-    Ok((data.memory.fn_endpoint.to_string(), map))
+    Ok((data.memory.fn_endpoint.to_string(), serde_json::to_string(&map).unwrap()))
 }
 
-fn construct_headers() -> HeaderMap {
-    let mut headers = HeaderMap::new();
-    let api_key = match env::var("FN_X_API_KEY") {
-        Ok(key) => HeaderValue::from_str(&key).unwrap(),
-        Err(_e) => HeaderValue::from_str("PoePoe").unwrap()
-    };
-
-    headers.insert(ACCEPT, HeaderValue::from_static("application/json"));
-    headers.insert(CONTENT_TYPE, HeaderValue::from_static("image/png"));
-    headers.insert("X-Api-Key", api_key);
-    headers
-}
-
-pub fn api(args: HashMap<String, Literal>, interval: Interval, data: &mut Data) -> Result<Literal, ErrorInfo> {
+pub fn api(args: HashMap<String, Literal>, _interval: Interval, data: &mut Data) -> Result<Literal, ErrorInfo> {
     let (http_arg, map) = parse_api(&args, data)?;
-    let client = ClientBuilder::new()
-            .use_rustls_tls()
-            .danger_accept_invalid_certs(true)
-            .build().unwrap();
+    let mut data = map.as_bytes();
+    // let client = ClientBuilder::new().build().unwrap();
 
-    // println!("http call {:?}", http_arg);
-    // println!("map {:?}", serde_json::to_string(&map).unwrap());
-    match client.post(&http_arg)
-        .headers(construct_headers())
-        .json(&map).send() {
+    // match client.post(&http_arg)
+    //     .headers(construct_headers())
+    //     .json(&map).send() {
 
-        Ok(ref mut arg) => match &arg.text() {
-            Ok(text) => {
-                // println!("reqwest post ok: ");
-                let json: serde_json::Value = serde_json::from_str(&text).unwrap();
-                if let Some(Value::String(val)) = json.get("data") {
-                    Ok(Literal::string(val.to_string()))
-                } else {
-                    Ok(Literal::null())
-                }
-            }
-            Err(_e) => {
-                Err(ErrorInfo{
-                    message: "Error in parsing reqwest result".to_owned(),
-                    interval
-                })
-            }
-        },
-        Err(_e) => {
-            Err(ErrorInfo{
-                message: "Error in reqwest post".to_owned(),
-                interval
-            })
-        }
+    //     Ok(ref mut arg) => match &arg.text() {
+    //         Ok(text) => {
+    //             // println!("reqwest post ok: ");
+    //             let json: serde_json::Value = serde_json::from_str(&text).unwrap();
+    //             if let Some(Value::String(val)) = json.get("data") {
+    //                 Ok(Literal::string(val.to_string()))
+    //             } else {
+    //                 Ok(Literal::null())
+    //             }
+    //         }
+    //         Err(_e) => {
+    //             Err(ErrorInfo{
+    //                 message: "Error in parsing reqwest result".to_owned(),
+    //                 interval
+    //             })
+    //         }
+    //     },
+    //     Err(_e) => {
+    //         Err(ErrorInfo{
+    //             message: "Error in reqwest post".to_owned(),
+    //             interval
+    //         })
+    //     }
+    // }
+
+    let mut result = Vec::new();
+    let mut easy = Easy::new();
+    easy.url(&http_arg).unwrap();
+    easy.post(true).unwrap();
+    easy.post_field_size(data.len() as u64).unwrap();
+    {
+        let mut list = List::new();
+        list.append("Accept: application/json").unwrap();
+        list.append("Content-Type: application/json").unwrap();
+        match env::var("FN_X_API_KEY") {
+            Ok(key) => list.append(&format!("X-Api-Key: {}", key) ).unwrap(),
+            Err(_e) =>  list.append("X-Api-Key: PoePoe").unwrap()
+        };
+        
+        easy.http_headers(list).unwrap();
+
+        let mut transfer = easy.transfer();
+        transfer.read_function(|buf| {
+            Ok(data.read(buf).unwrap_or(0))
+        }).unwrap();
+
+        transfer.write_function(|new_data| {
+            result.extend_from_slice(new_data);
+            Ok(new_data.len())
+        }).unwrap();
+
+        transfer.perform().unwrap();
+    }
+
+    let json: serde_json::Value = serde_json::from_str(& String::from_utf8_lossy(&result)).unwrap();
+    if let Some(Value::String(val)) = json.get("data") {
+        Ok(Literal::string(val.to_string()))
+    } else {
+        Ok(Literal::null())
     }
 }
