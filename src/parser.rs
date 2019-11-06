10b4201c34f41136fa22db8c8c1820c0029ea631
Alexis Merelo <alexis.merelo@clevy.io>
2019-11-06 14:10:39 +0100

    fix warning

--- src/parser.rs
+++ src/parser.rs
@@ -73,7 +73,7 @@ impl Parser {
                         }
                     )
                 },
-                Err::Incomplete(err) => unimplemented!(),
+                Err::Incomplete(_err) => unimplemented!(),
             },
         }
     }
