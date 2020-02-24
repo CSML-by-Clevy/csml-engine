fd955079d43cee9e0b80b2b2f4cd0ad34a9ff57f
Alexis Merelo <alexis.merelo@clevy.io>
2020-02-24 14:47:01 +0100

    add built in wait test

---
+++ tests/wait.rs
@@ -0,0 +1,42 @@
+mod support;
+
+use csmlinterpreter::data::{Event, MessageData};
+use csmlinterpreter::interpret;
+use serde_json::Value;
+
+use support::tools::{gen_context, message_to_jsonvalue, read_file};
+
+fn format_message(event: Option<Event>, file: &str, step: &str) -> MessageData {
+    let text = read_file(format!("CSML/built-in/{}.csml", file)).unwrap();
+
+    let context = gen_context(
+        serde_json::json!({}),
+        serde_json::json!({}),
+        0,
+        false,
+    );
+
+    interpret(&text, step, context, &event, None, None, None)
+}
+
+#[test]
+fn wait_0() {
+    let data = r#"{"messages":[ {"content":{"error": "Builtin Wait expect one argument of type int or float | example: Wait(3) at line 5, column 6"},"content_type":"error"} ],"next_flow":null,"memories":[],"next_step":null}"#;
+    let msg = format_message(None, "wait", "wait_0");
+
+    let v1: Value = message_to_jsonvalue(msg);
+    let v2: Value = serde_json::from_str(data).unwrap();
+
+    assert_eq!(v1, v2)
+}
+
+#[test]
+fn wait_1() {
+    let data = r#"{"messages":[ {"content":{"duration": 10},"content_type":"wait"} ],"next_flow":null,"memories":[],"next_step":"end"}"#;
+    let msg = format_message(None, "wait", "wait_1");
+
+    let v1: Value = message_to_jsonvalue(msg);
+    let v2: Value = serde_json::from_str(data).unwrap();
+
+    assert_eq!(v1, v2)
+}
