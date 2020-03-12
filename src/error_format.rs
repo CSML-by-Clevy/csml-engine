ea15819a1cbeb889f8e0d1945b8b25a99cb7757d
Alexis Merelo <alexis.merelo@clevy.io>
2020-03-12 14:05:21 +0100

    update error messages

--- src/error_format.rs
+++ src/error_format.rs
@@ -9,25 +9,22 @@ use nom::{
 pub use data::{CustomError, ErrorInfo};
 
 pub const ERROR_PARENTHESES: &'static str = "list elem type ( ... ) not found";
-pub const ERROR_PARENTHESES_END: &'static str = "invalid element expext ',' or ')'";
+pub const ERROR_PARENTHESES_END: &'static str = "invalid argument expect one ',' between each argument or ')' to end the list";
 pub const ERROR_NUMBER_AS_IDENT: &'static str = "int/float can't be used as identifier";
 pub const ERROR_RESERVED: &'static str = "reserved keyword can't be used as identifier";
 pub const ERROR_PARSING: &'static str =
-    "Bad keyword please use one of [say, do, if, ..] keywords to start an actoion";
-pub const ERROR_REMEMBER: &'static str = "Remember must be assigning to a variable via '=' or 'as': remember key = value || remember value as key";
+    "invalid argument use one of [say, do, if, ..] keywords to start an action";
+pub const ERROR_REMEMBER: &'static str = "remember must be assigning to a variable via '=': remember key = value";
 pub const ERROR_USE: &'static str =
-    "Use must be assigning to a variable via 'as': use value as key";
-pub const ERROR_BREAK: &'static str = "Break can only be used inside a foreach";
-pub const ERROR_HOLD: &'static str = "Hold cannot be used inside a foreach";
+    "use must be assigning to a variable via 'as': use value as key";
+pub const ERROR_BREAK: &'static str = "break can only be used inside a foreach";
+pub const ERROR_HOLD: &'static str = "hold cannot be used inside a foreach";
 pub const ERROR_LEFT_BRACE: &'static str = "expect '('";
 pub const ERROR_RIGHT_BRACE: &'static str = "expect ')'";
-// pub const ERROR_LEFT_BRACKET: &'static str = "expect '['";
 pub const ERROR_RIGHT_BRACKET: &'static str = "expect ']'";
 pub const ERROR_GOTO_STEP: &'static str = "missing step name after goto";
 pub const ERROR_IMPORT_STEP: &'static str = "missing step name after import";
 pub const ERROR_DOUBLE_QUOTE: &'static str = "expect '\"' to end string";
-
-// ]
 
 pub fn format_error<I>(
     interval: Interval,
