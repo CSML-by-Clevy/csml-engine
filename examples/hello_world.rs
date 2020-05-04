5803e99181949fb5f0ab0c5e04290f6d1d052bf0
jle-quel <jle-quel@student.42.us.org>
2020-05-04 10:00:42 +0200

    [fix]: rewrite hello_world example

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,82 +1,44 @@
-// use csmlinterpreter::{
-//     data::{ContextJson, Event, MessageData},
-//     interpret,
-// };
-// use serde_json::{json, map::Map, Value};
+use csmlinterpreter::data::csml_bot::CsmlBot;
+use csmlinterpreter::data::csml_flow::CsmlFlow;
+use csmlinterpreter::data::event::Event;
+use csmlinterpreter::data::ContextJson;
+use csmlinterpreter::interpret;
 
-// use std::fs::File;
-// use std::io::prelude::*;
+const DEFAULT_ID_NAME: &str = "id";
+const DEFAULT_FLOW_NAME: &str = "default";
+const DEFAULT_STEP_NAME: &str = "start";
+const DEFAULT_BOT_NAME: &str = "my_bot";
 
-// fn read_file(file_path: String) -> Result<String, ::std::io::Error> {
-//     let mut file = File::open(file_path)?;
-//     let mut contents = String::new();
-//     file.read_to_string(&mut contents)?;
-//     Ok(contents)
-// }
-
-// pub fn format_message(result: MessageData) -> Value {
-//     let mut message: Map<String, Value> = Map::new();
-//     let mut vec = vec![];
-//     let mut memories = vec![];
-
-//     for msg in result.messages.iter() {
-//         vec.push(msg.to_owned().message_to_json());
-//     }
-
-//     if let Some(mem) = result.memories {
-//         for elem in mem.iter() {
-//             let mut map = Map::new();
-//             map.insert(elem.key.to_owned(), elem.value.to_owned());
-//             memories.push(json!(map));
-//         }
-//     }
-
-//     message.insert("memories".to_owned(), Value::Array(memories));
-//     message.insert("messages".to_owned(), Value::Array(vec));
-//     message.insert(
-//         "next_flow".to_owned(),
-//         match serde_json::to_value(result.next_flow) {
-//             Ok(val) => val,
-//             _ => json!(null),
-//         },
-//     );
-//     message.insert(
-//         "next_step".to_owned(),
-//         match serde_json::to_value(result.next_step) {
-//             Ok(val) => val,
-//             _ => json!(null),
-//         },
-//     );
-//     Value::Object(message)
-// }
-
-// fn interpret_flow(flow: &str, step_name: &str) {
-//     let event = Event::text("hello");
-//     let mut metadata = Map::new();
-
-//     metadata.insert("firstname".to_owned(), json!("Alexis"));
-
-//     metadata.insert("mavar".to_owned(), json!(10));
-
-//     let mut obj = Map::new();
-//     obj.insert("var1".to_owned(), json!(1));
-//     obj.insert("var2".to_owned(), json!(42));
-//     metadata.insert("obj".to_owned(), json!(obj));
-
-//     let context = ContextJson {
-//         current: serde_json::json!({}),
-//         metadata: json!(metadata),
-//         api_info: None,
-//         hold: None,
-//     };
-
-//     dbg!(format_message(interpret(
-//         flow, step_name, context, &event, None,
-//     )));
-// }
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC FUNCTION
+////////////////////////////////////////////////////////////////////////////////
 
 fn main() {
-    //     let flow = read_file("CSML/examples/hello_world.csml".to_owned()).unwrap();
+    let default_content = std::fs::read_to_string("CSML/examples/hello_world.csml").unwrap();
+    let default_flow = CsmlFlow::new(DEFAULT_ID_NAME, "default", &default_content, Vec::default());
 
-    //     interpret_flow(&flow, "start");
+    // Create a CsmlBot
+    let bot = CsmlBot::new(
+        DEFAULT_ID_NAME,
+        DEFAULT_BOT_NAME,
+        None,
+        vec![default_flow],
+        DEFAULT_FLOW_NAME,
+    );
+
+    // Create an Event
+    let event = Event::default();
+
+    // Create context
+    let context = ContextJson::new(
+        serde_json::json!({}),
+        serde_json::json!({}),
+        None,
+        None,
+        DEFAULT_STEP_NAME,
+        DEFAULT_FLOW_NAME,
+    );
+
+    // Run interpreter
+    dbg!(interpret(bot, context, event, None));
 }
