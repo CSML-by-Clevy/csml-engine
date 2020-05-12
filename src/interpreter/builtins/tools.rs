1c79825a6e8dae46f97b585fc6917ada9845e7ed
Alexis Merelo <alexis.merelo@clevy.io>
2020-05-12 13:20:48 +0200

    replace curl by ureq

--- src/interpreter/builtins/tools.rs
+++ src/interpreter/builtins/tools.rs
@@ -1,37 +1,36 @@
-use crate::data::primitive::array::PrimitiveArray;
+use crate::data::primitive::{PrimitiveArray, PrimitiveString};
 use crate::data::{Client, Interval, Literal};
 use crate::error_format::*;
-use serde_json::{Map, Value};
 use std::collections::HashMap;
 use std::hash::BuildHasher;
 
 pub fn create_submap<S: BuildHasher>(
     keys: &[&str],
     args: &HashMap<String, Literal, S>,
-) -> Result<Map<String, Value>, ErrorInfo> {
-    let mut map = Map::new();
+) -> Result<HashMap<String, Literal>, ErrorInfo> {
+    let mut map = HashMap::new();
 
     for elem in args.keys() {
         if keys.iter().find(|&&x| x == elem).is_none() {
             if let Some(literal) = args.get(&*elem) {
-                map.insert(elem.clone(), literal.primitive.to_json());
+                map.insert(elem.clone(), literal.clone());
             }
         }
     }
     Ok(map)
 }
 
-pub fn client_to_json(client: &Client) -> Map<String, Value> {
-    let mut map = Map::new();
+pub fn client_to_json(client: &Client, interval: Interval) -> HashMap<String, Literal> {
+    let mut map = HashMap::new();
 
-    map.insert("bot_id".to_owned(), Value::String(client.bot_id.to_owned()));
+    map.insert("bot_id".to_owned(), PrimitiveString::get_literal(&client.bot_id, interval));
     map.insert(
         "channel_id".to_owned(),
-        Value::String(client.channel_id.to_owned()),
+        PrimitiveString::get_literal(&client.channel_id, interval),
     );
     map.insert(
         "user_id".to_owned(),
-        Value::String(client.user_id.to_owned()),
+        PrimitiveString::get_literal(&client.user_id, interval),
     );
 
     map
