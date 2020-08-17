afe0e5d0a83fd74179f5dce4630cd6844bd6d21f
Alexis Merelo <alexis.merelo@clevy.io>
2020-08-17 14:11:05 +0200

    [fix]: components dir path

--- csml_interpreter/src/interpreter/builtins/components.rs
+++ csml_interpreter/src/interpreter/builtins/components.rs
@@ -1,9 +1,25 @@
 use crate::data::error_info::ErrorInfo;
-use std::fs;
+use std::path::Path;
+use std::{fs, env};
 use std::io::prelude::*;
 
+// TODO: tmp while there is no CARGO_WORKSPACE_MANIFEST_DIR
+fn get_components_dir() -> String {
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let mut ancestors = Path::new(&manifest_dir).ancestors();
+
+    while let Some(path) = ancestors.next() {
+        let tmp = path.join("components");
+        if tmp.exists(){
+            return format!("{}", tmp.display()) ;
+        }
+    }
+
+    panic!("components_dir not found")
+}
+
 pub fn read() -> Result<serde_json::Map<String, serde_json::Value>, ErrorInfo> {
-    let paths = fs::read_dir("../components/")?;
+    let paths = fs::read_dir(get_components_dir())?;
     let mut components = serde_json::Map::new();
 
     for path in paths {
