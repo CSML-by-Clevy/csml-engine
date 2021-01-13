e9880ddb1d019cfb0d5699b724dfd471757f13ad
François Falala-Sechet <francois@clevy.io>
2021-01-13 22:33:46 +0100

    [chore] routes should be more REST-compliant

--- csml_server/src/routes.rs
+++ csml_server/src/routes.rs
@@ -4,7 +4,4 @@ pub mod run;
 pub mod sns;
 pub mod conversations;
 
-pub mod create_bot_version;
-pub mod get_last_bot_version;
-pub mod get_bot_by_version_id;
-pub mod get_bot_versions;
+pub mod bot_versions;
