86e09dfc8b5d97ebbc672e3151baee89b4588386
Alexis Merelo <alexis.merelo@clevy.io>
2021-02-23 16:17:49 +0100

    [add] base64/hex encoce/decode

---
+++ csml_interpreter/tests/base64.rs
@@ -0,0 +1,26 @@
+mod support;
+
+use csml_interpreter::data::context::Context;
+use csml_interpreter::data::event::Event;
+use std::collections::HashMap;
+
+use crate::support::tools::format_message;
+use crate::support::tools::message_to_json_value;
+
+use serde_json::Value;
+
+#[test]
+fn base64_0() {
+    let data =
+        r#"{"memories":[], "messages":[ {"content":{"text": "Hello World"},"content_type":"text"}  ]}"#;
+    let msg = format_message(
+        Event::new("payload", "", serde_json::json!({})),
+        Context::new(HashMap::new(), HashMap::new(), None, None, "start", "flow"),
+        "CSML/basic_test/built-in/base64.csml",
+    );
+
+    let v1: Value = message_to_json_value(msg);
+    let v2: Value = serde_json::from_str(data).unwrap();
+
+    assert_eq!(v1, v2)
+}
