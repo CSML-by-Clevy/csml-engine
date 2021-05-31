4f120092a2ef942c558a54042bbe5caf84ba8d06
Alexis Merelo <alexis.merelo@clevy.io>
2021-05-31 15:47:14 +0200

    format infinite loop error message

---
+++ csml_interpreter/tests/infinite_loop.rs
@@ -0,0 +1,23 @@
+mod support;
+
+use csml_interpreter::data::context::Context;
+use csml_interpreter::data::event::Event;
+use std::collections::HashMap;
+
+use crate::support::tools::format_message;
+use crate::support::tools::message_to_json_value;
+
+use serde_json::Value;
+
+#[test]
+fn infinite_loop() {
+    let msg = format_message(
+        Event::new("payload", "", serde_json::json!({})),
+        Context::new(HashMap::new(), HashMap::new(), None, None, "start", "flow"),
+        "CSML/basic_test/infinite_loop.csml",
+    );
+
+    let error: Value = message_to_json_value(msg);
+
+    assert_eq!("error", error["messages"][0]["content_type"])
+}
