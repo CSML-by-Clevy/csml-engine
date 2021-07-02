4ffcac7736893d90f12f85f2dea1c157700563d4
Alexis Merelo <alexis.merelo@clevy.io>
2021-07-02 14:42:12 +0200

    add key rotation

--- csml_server/src/routes/tools.rs
+++ csml_server/src/routes/tools.rs
@@ -5,10 +5,12 @@ pub fn validate_api_key(req: &actix_web::HttpRequest) -> Option<String> {
       _ => return None
     };
 
+    let vec = api_key.split(',').collect::<Vec<&str>>();
+
     match req.headers().get("X-Api-Key") {
       Some(val) => {
         let val = val.to_str().unwrap_or("");
-        if val.is_empty() || val != api_key {
+        if val.is_empty() || !vec.contains(&val) {
           eprintln!("EngineError: wrong X-Api-Key value [{}]", val);
           return Some(val.to_owned())
         }
